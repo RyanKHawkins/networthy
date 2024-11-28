8b104c18064db78e3e37a7ecd2e7286b2c4b4f2e
Ryan Hawkins <rkhawkins80@hotmail.com>
2024-11-28 09:55:38 -0600

    added new functions to export

--- helper.js
+++ helper.js
@@ -2,8 +2,11 @@
 
 function convertPercentageToDecimal(percent) {
     return percent / 100
-}
+};
 
 function convertDecimalToPercentage (decimal) {
     return decimal * 100
-}
+};
+
+
+export {convertPercentageToDecimal, convertDecimalToPercentage};
